ef238095e3936876f0bb7b0238ea0363a9f41096
Andrea <andreamartinezdl1@gmail.com>
2026-02-03 21:32:53 -0600

    Ejercicio done!

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ if (todosIguales(numbers)) {
     console.log("Todos los números son iguales: " + numbers[0]);
 } else {    
     // Ordenamos de menor a mayor usando una función comparadora
-    // Usamos [...numbers] para crear una copia y no alterar el original accidentalmente
     const menorAMayor = [...numbers].sort((a, b) => a - b);
     const mayorAMenor = [...numbers].sort((a, b) => b - a);
 
     console.log("Menor a mayor:", menorAMayor);
     console.log("Mayor a menor:", mayorAMenor);
-    
-    // Identificar posiciones específicas
-    console.log(`Mayor: ${mayorAMenor[0]}, Centro: ${mayorAMenor[1]}, Menor: ${mayorAMenor[2]}`);
 }
